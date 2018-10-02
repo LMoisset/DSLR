d6883646be425805ae185dee24090cf4a6e64cf1
Lola MOISSET <lmoisset@e2r2p21.42.fr>
2018-10-02 15:23:03 +0200

    reading the data

--- describe.py
+++ describe.py
@@ -1,1 +1,11 @@
 #! /usr/bin/python
+import os
+
+def read_data(dataname):
+    with open("data/" + dataname) as f:
+        lis=[line.split() for line in f]        # create a list of lists
+        for i,x in enumerate(lis):              #print the list items
+            print "line{0} = {1}".format(i,x)
+
+read_data('dataset_train.csv')
+### Data Analysis
