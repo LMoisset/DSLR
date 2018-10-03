39234c3220c40fd71146e85adef5ca33b5f93662
mathildelavacquery <mathilde.lavacquery@polytechnique.edu>
2018-10-03 22:20:42 +0200

    version finale

--- histogram.py
+++ histogram.py
@@ -2,7 +2,8 @@
 
 import argparse
 import matplotlib.pyplot as plt
-from describe import Quartile, Count
+import matplotlib.gridspec as gridspec
+from describe import Quartile, Count, Mean
 
 
 ### Quel cours de Poudlard a une repartition des notes homogenes entre les 4 maisons ?
@@ -22,7 +23,28 @@ def read_data2(dataname):
                     feature_dico[feature_list[i]][house] = [grades[i]]
         return feature_dico, feature_list
 
-def freq_per_house(feature): # a dictionnary and its keys
+
+def anova4(x, y, z, t):
+    x = [float(l) for l in x if l]
+    y = [float(l) for l in y if l]
+    z = [float(l) for l in z if l]
+    t = [float(l) for l in t if l]
+    tot = x + y +z + t
+    tot_nest = [x, y, z, t]
+    SSbetween = - sum(tot)**2/float(len(tot))
+    for i in range(4):
+        SSbetween += sum(tot_nest[i])**2/float(len(tot_nest[i]))
+    SSwithin  = sum([l**2 for l in tot]) - sum(tot)**2/float(len(tot))
+    dfb = len(tot_nest) - 1
+    dfw = len(tot)- len(tot_nest)
+    MSbetween = SSbetween/ dfb
+    MSwithin = SSwithin / dfw
+    F = MSbetween /MSwithin
+    return F
+
+
+
+def freq_per_house(feature, b = 20): # a dictionnary / b = nb of bins, an integer
     houses = feature.keys()
     # distribution of the grades for everyone
     all_grades = []
@@ -30,18 +52,28 @@ def freq_per_house(feature): # a dictionnary and its keys
         all_grades += feature[house]
     mini = Quartile(all_grades, 0) #min
     maxi = Quartile(all_grades, 1) #max
-    grade_list = [mini + (maxi-mini)/10*i for i in range(11)]
+    grade_list = [mini + (maxi-mini)/b*i for i in range(b+1)]
     # grade dico per range and per house
-    grade_dico = dict((grade, dict()) for grade in grade_list)
+    #grade_dico = dict((house, dict()) for house in houses)
+    xy_dico = dict((house, dict()) for house in houses)
     for house in houses:
+        xy_dico[house]['x'] = [mini]
+        xy_dico[house]['y'] = [0]
         lis = [float(l) for l in feature[house] if l]
         nb_student = Count(lis) # nb of students per house enrolled in the class
-        print nb_student
-        for i in range(1,11):
+        for i in range(1,b+1):
             lis2 = [l for l in lis if (l <= grade_list[i] and l > grade_list[i-1])]
-            grade_dico[grade_list[i]][house] = Count(lis2)/ float(nb_student) # count nb of grades in the range
-    return grade_dico
+            freq = Count(lis2)/float(nb_student)
+            #grade_dico[grade_list[house]][i] = freq 
+            xy_dico[house]['x'].extend((grade_list[i-1], grade_list[i]))
+            xy_dico[house]['y'].extend((freq, freq))
+        xy_dico[house]['x'].append(grade_list[b]) # pour refermer le graph
+        xy_dico[house]['y'].append(0)
+    score_hg = 0
+    return xy_dico
 
+
+    
 
 
 
@@ -49,22 +81,50 @@ def freq_per_house(feature): # a dictionnary and its keys
 
 house_colors = {'Ravenclaw': 'blue', 'Slytherin': 'green', 'Gryffindor' : 'red', 'Hufflepuff' : 'yellow'}
 
+# ANOVA F stat with confidence alpha = 0.05
+# grade repartition is homogeneous if F <= F_3_1550
+F_3_1550 = 2.6
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Dataset you want to describe')
     parser.add_argument('set', type = str, help = 'Name of the file to read')
     args = parser.parse_args()
     feature_dico, feature_list = read_data2(args.set)
-    houses = feature_dico[feature_list[0]].keys()
+    houses = list(feature_dico[feature_list[0]].keys())
 
-    print feature_dico[feature_list[0]][houses[0]][1:3]
+    
+    homogen_features = []
+    for feature in feature_list:
+        X = feature_dico[feature]
+        F = anova4(X[houses[0]],X[houses[1]], X[houses[2]], X[houses[3]])
+        # print(F)
+        if F <= F_3_1550:
+            homogen_features.append(feature)
 
-    grade_dico = freq_per_house(feature_dico[feature_list[1]])
-    print grade_dico
 
-    # for feature in feature_list:
-    #     print feature
-    #     print feature_dico[feature][10:12]
-    plt.plot([1, 2, 3, 4], [10, 20, 25, 30], color='lightblue', linewidth=3)
-    plt.scatter([0.3, 3.8, 1.2, 2.5], [11, 25, 9, 26], color='darkgreen', marker='^')
-    plt.xlim(0.5, 4.5)
+    k = 0
+    fig = plt.figure(figsize = (20,10), dpi = 100)
+    grid = gridspec.GridSpec(1, len(homogen_features))
+    
+    for feature in homogen_features:
+        xy_dico = freq_per_house(feature_dico[feature], 20)
+        plt.subplot(grid[0, k])
+        
+        for house in houses:
+            x = xy_dico[house]['x']
+            y = xy_dico[house]['y']
+            plt.fill(x, y, color= house_colors[house], linewidth=2, label = house, alpha = 0.5)
+        plt.xlim(Quartile(x,0),)
+        plt.ylim(Quartile(y,0),)
+        plt.title(feature_list[k])
+        plt.legend(ncol = 2, fontsize = 'x-small')
+        k += 1
+
+    plt.tight_layout()
     plt.show(block = True)
+
+
+
+
+
