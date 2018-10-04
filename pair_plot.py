07bad5bb323dd37404942697afd7af20c5691829
mathildelavacquery <mathilde.lavacquery@polytechnique.edu>
2018-10-04 15:03:33 +0200

    add legend

--- pair_plot.py
+++ pair_plot.py
@@ -5,21 +5,10 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from describe import Quartile, Count
 from histogram import convert_float, read_data2, freq_per_house, plot_hist
+from scatter_plot import scatter_plot
 
 
 house_colors = {'Ravenclaw': 'blue', 'Slytherin': 'green', 'Gryffindor' : 'red', 'Hufflepuff' : 'yellow'}
-
-
-def scatter_plot(dico, feature1, feature2, house_colors):
-    list1 = dico[feature1]
-    list2 = dico[feature2]
-    for house in house_colors.keys():
-        x = list1[house]
-        y = list2[house]
-        plt.plot(x, y, 'o',color = house_colors[house], label = house, markersize=0.5)
-    #plt.xlabel(feature1)
-    #plt.ylabel(feature2)
-
 
 def pair_plot(feature_dico, feature_list, house_colors):
     n = len(feature_list)
@@ -30,17 +19,16 @@ def pair_plot(feature_dico, feature_list, house_colors):
             plt.subplot(grid[k,l]) 
             if k == l:
                 plot_hist(feature_dico, feature_list[k], 20, house_colors)
-                if k == n:
+                if k == 0:
                     plt.legend(ncol = 2, fontsize = 'x-small')
             else:
-                scatter_plot(feature_dico, feature_list[k], feature_list[l], house_colors)
+                plot = scatter_plot(feature_dico, feature_list[k], feature_list[l], house_colors)
             if k == 0:
                 plt.title(feature_list[l])
             if l == 0:
                 plt.ylabel(feature_list[k])
     plt.tight_layout()
     plt.show(block = True)
-
 
 
 if __name__ == '__main__':
@@ -50,6 +38,6 @@ if __name__ == '__main__':
     feature_dico, feature_list = read_data2(args.set)
     houses = list(feature_dico[feature_list[0]].keys())
 
-    feature_list2 = feature_list[:6]
+    feature_list2 = feature_list[:5]
 
     pair_plot(feature_dico, feature_list2, house_colors)
