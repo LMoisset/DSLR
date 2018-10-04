302879cbbf12104f7705dabc52c1611bcaddb339
mathildelavacquery <mathilde.lavacquery@polytechnique.edu>
2018-10-04 15:13:59 +0200

    ok

--- histogram_tot.py
+++ histogram_tot.py
@@ -3,47 +3,38 @@
 import argparse
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-from describe import Quartile, Count, Mean
-from histogram import read_data2, freq_per_house
+from describe import Quartile, Count
+from histogram import convert_float, read_data2, freq_per_house, plot_hist
 
-house_colors = {'Ravenclaw': 'blue', 'Slytherin': 'green', 'Gryffindor' : 'red', 'Hufflepuff' : 'yellow'}
+
+def hist_tot(feature_dico, feature_list, b, house_colors):
+    l = 0
+    m = 0
+    fig = plt.figure(figsize = (20,10), dpi = 100)
+    grid = gridspec.GridSpec(4,4)
+    for k in range(len(feature_list)):
+        xy_dico = freq_per_house(feature_dico[feature_list[k]], b)
+        if k <4:
+            l = k
+        else:
+            m = k//4
+            l = k - 4*m
+        plt.subplot(grid[m, l])
+        plot_hist(feature_dico, feature_list[k], b, house_colors)
+        plt.title(feature_list[k])
+        plt.legend(ncol = 2, fontsize = 'x-small')
+    plt.tight_layout()
+    plt.show(block = True)
+    
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Dataset you want to describe')
     parser.add_argument('set', type = str, help = 'Name of the file to read')
     args = parser.parse_args()
+
+    house_colors = {'Ravenclaw': 'blue', 'Slytherin': 'green', 'Gryffindor' : 'red', 'Hufflepuff' : 'yellow'}
+
     feature_dico, feature_list = read_data2(args.set)
-    houses = list(feature_dico[feature_list[0]].keys())
 
-
-    l = 0
-    m = 0
-    fig = plt.figure(figsize = (20,10), dpi = 100)
-    grid = gridspec.GridSpec(4,4)
-    
-    for k in range(len(feature_list)):
-        xy_dico = freq_per_house(feature_dico[feature_list[k]], 20)
-        if k <4:
-            l = k
-        else:
-            m = k//4
-            l = k - 4*m
-        
-        plt.subplot(grid[m, l])
-        
-        for house in houses:
-            x = xy_dico[house]['x']
-            y = xy_dico[house]['y']
-            plt.fill(x, y, color= house_colors[house], linewidth=2, label = house, alpha = 0.5)
-        plt.xlim(Quartile(x,0),)
-        plt.ylim(Quartile(y,0),)
-        plt.title(feature_list[k])
-        plt.legend(ncol = 2, fontsize = 'x-small')
-    plt.tight_layout()
-    plt.show(block = True)
-
-
-
-
-
+    hist_tot(feature_dico, feature_list, 20, house_colors)
