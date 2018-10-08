26fb791f47551d2206d6b11c35ff3e64663e772a
mathildelavacquery <mathilde.lavacquery@polytechnique.edu>
2018-10-08 11:54:45 +0200

    errors fixed for the preprocessing part

--- logreg_train.py
+++ logreg_train.py
@@ -60,68 +60,74 @@ class Matrix(list):
 
     def drop(self, k_list, axis):
         if axis == 0: # rows
-            mat = Matrix([self[i] for i in [x for x in range(self.nrow) if x != k_list[0]]]) # initialisation
-            if len(k_list) > 1:
-                for k in k_list:
-                    mat = Matrix([self[i] for i in [x for x in range(self.nrow) if x != k]])
-        elif axis ==1: # columns
-            mat = Matrix([[row[i] for row in self] for i in [x for x in range(self.ncol) if x != k_list[0]]])
-            if len(k_list) >1:
-                for k in k_list:
-                    mat = Matrix([[row[i] for row in mat] for i in [x for x in range(self.ncol) if x != k]])
+            mat = Matrix([self[i] for i in [k for k in range(self.nrow) if k not in k_list]])
+        elif axis == 1: # columns
+            mat = Matrix([[row[i] for i in [k for k in range(self.ncol) if k not in k_list]] for row in self])
         return mat
 
     def to_float(self):
-        return Matrix([[float(row[i]) if row[i].replace('.', '',1).isdigit() else row[i] for i in range(self.ncol)] for row in self])
+        return Matrix([[float(row[i]) if row[i].replace('-','',1).replace('.', '',1).isdigit() else row[i] for i in range(self.ncol)] for row in self])
 
     def unique(self, i, axis):
-        res = []
+        unique_values = []
         if axis == 0:
             for k in range(self.ncol):
-                if self[i][k] not in res:
-                    res.append(self[i][k])
+                if self[i][k] not in unique_values:
+                    unique_values.append(self[i][k])
         elif axis == 1:
             for k in range(self.nrow):
-                if self[k][i] not in res:
-                    res.append(self[k][i])
-        return res                 
+                if self[k][i] not in unique_values:
+                    unique_values.append(self[k][i])
+        return unique_values
+    
+    def count_null(self):
+        null_dico = dict((k, 0) for k in range(self.ncol))
+        for i in range(self.ncol):
+            for j in range(self.nrow):
+                if self[j][i] == '':
+                    null_dico[i] +=1
+        return null_dico  
+
 
         
-
-
-
 def read_data3(dataname):
     with open('../data/' + dataname) as f:
         return Matrix([line.strip().split(',') for line in f])
 
-def cat_to_dummies(X): ### ATTENTION PB  --- pas le nom des colonens, on va se perdre dans les features
+def cat_to_dummies(X, features): 
     ind_to_drop = []
     for i in range(X.ncol):
-        cat = X.col(i).unique()
+        cat = X.unique(i, axis = 1)
         if len(cat) <5: # si moins de 5 uniques, on assume que la var est cat
             ind_to_drop.append(i)
             for j in range(len(cat)-1):
+                new_cat = features[i] + '_' + cat[j]
                 new_col = [1 if x == cat[j] else 0 for x in [row[i] for row in X]]
                 X.append_col(new_col)
-    X.drop(ind_to_drop, axis = 1)
-    return X
+                features.append(new_cat)
+    X = X.drop(ind_to_drop, axis = 1)
+    features = [features[i] for i in range(len(features)) if i not in ind_to_drop]
+    return X, features
 
-def impute_na(X): # pour l'instant mean imputation/ pk pas EM si temps
-    return Matrix([Mean([[row[i] for row in X]) if row[i] =='' else row[i] for i in range(self.ncol)] for row in self])
+def impute_na(X): # pour l'instant mean imputation
+    return Matrix([[Mean([row[i] for row in X]) if row[i] =='' else row[i] for i in range(X.ncol)] for row in X])
 
 def preprocess(dataname):
-    df = read_data3('../data/'+dataname, sep = ',')
+    df = read_data3('../data/'+dataname)
     Y = df.col(1)
-    X = df.drop([0,1,2, 3,4], axis = 1) # get rid of index, name, birthday
+    print(df[0])
+    X = df.drop([0,1,2,3,4], axis = 1) # get rid of index, house, firstname, lastname, birthday
     features = X[0]
     X = X.drop([0], axis = 0)
     X = X.to_float()
     # 1rst, we transform categorical variables into dummies
-    X_2 = cat_to_dummies(X)
+    X_2, features = cat_to_dummies(X,features)
+    print(X_2.count_null())
     # 2nd, handling Missing Values
     X_3 = impute_na(X_2)
+    print(X_3.count_null())
     # get rid of correlated data 
-    return X_2, Y, features
+    return X_3, Y, features
 
 
 def g(z):
@@ -136,8 +142,7 @@ def loss_function(X, Y, theta): # X is an array, Y a column array
     m = len(Y)
     J = 0
     for i in range(m):
-        X_t = X[[i]].transpose()
-        J += -1/m * (Y[i]*log(h(X_t, theta))) + (1-Y[i])*log(1-h(X_t, theta))
+        J += -1/m * (Y[i]*log(h(X.col(i), theta))) + (1-Y[i])*log(1-h(X.col(i), theta))
     return J
 
 def gradient(X, Y, theta, j): # X is an array, Y a column array
@@ -166,19 +171,19 @@ if __name__ == '__main__':
 
     #X = read_data3(args.set)
     #print(X.drop([0], axis = 0)[0])
-    test = Matrix([['1','2.5', '']])
+    #test = Matrix([['1','2', '3'], ['4', '5', '6']])
     #t2 = [float(x) if x.replace('.', '',1).isdigit() else x for x in test]
-    print(test)
-    print(test.nrow, test.ncol)
-    print(test.to_float())
+    #test.show()
+    #test.drop([], axis = 1).show()
+    #print(test.unique(0, axis = 1))
+    
 
-
-    #X, Y, features = preprocess(args.set)
+    X, Y, features = preprocess(args.set)
+    print(features)
     #print(X)
-    #print(X[[0]])
     #print(Y)
     #print(Y[0])
-    #print(X[[0],[1]])
+    print(X[0])
     #print(X[[0]][0])
     #print(len(X))
 
